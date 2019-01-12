376880721dacb93aab23a6ae0927bdfc6d545c1e
ernii <ernii@amazon.com>
2019-01-12 11:22:02 -0500

    Initial commit

--- bot.js
+++ bot.js
@@ -1,38 +1,44 @@
+////////////////////////////////////////////////////////////////////////////////
 const Botkit = require('botkit');
 const { slackGetUsersInfo } = require('./lib/slackGetUsersInfo');
 const { chatOpsLogger } = require('./lib/chatOpsLogger');
+////////////////////////////////////////////////////////////////////////////////
 
+
+////////////////////////////////////////////////////////////////////////////////
+// Make sure we have a Slack token
 if (!process.env.token) {
     console.log('Error: Specify a Slack bot token in environment.');
     usage_tip();
     process.exit(1);
 }
 
-// Create the Botkit controller, which controls all instances of the bot
+// Create the Botkit controller
 let controller = Botkit.slackbot({
     debug: false,
     retry: 10,
     studio_token: process.env.studio_token
 });
 
-// Spawn a single instance of the bot to connect to your Slack team
+// Spawn a single instance of the bot and connect Slack
 let bot = controller.spawn({
     token: process.env.token,
 }).startRTM();
 
-
+// Load the skills
 let normalizedPath = require("path").join(__dirname, "skills");
 require("fs").readdirSync(normalizedPath).forEach(function (file) {
     require("./skills/" + file)(controller);
 });
 
-
+// Display CLI help
 function usage_tip() {
     console.log('Execute your bot application like this:');
     console.log('npm install');
     console.log('token=<MY SLACK TOKEN> node bot.js');
     console.log('Get a Slack token here: https://my.slack.com/apps/new/A0F7YS25R-bots');
 }
+////////////////////////////////////////////////////////////////////////////////
 
 
 
@@ -61,7 +67,6 @@ controller.hears(['help internet'], 'direct_message,direct_mention', (bot, messa
         }
     })();
 });
-
 
 ////////////////////////////////////////////////////////////////////////////////
 // BOT HELP GLOBAL CATCH ALL AND UNKNOWN SECTION //
